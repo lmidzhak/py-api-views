6ec40abaaea70afe6d3e4aa6879ca94fd5a14ccb
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-07-10 13:20:46 +0300

    change to serializer.is_valid(raise_exception=True) code in views.py

--- cinema/views.py
+++ cinema/views.py
@@ -21,10 +21,9 @@ class GenreList(APIView):
 
     def post(self, request) -> Response:
         serializer = GenreSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GenreDetail(APIView):
@@ -38,16 +37,14 @@ class GenreDetail(APIView):
     def put(self, request, pk: int) -> Response:
         serializer = GenreSerializer(self.get_object(pk=pk), data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(raise_exception=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk: int) -> Response:
         serializer = GenreSerializer(self.get_object(pk=pk), data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(raise_exception=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk: int) -> Response:
         self.get_object(pk=pk).delete()
